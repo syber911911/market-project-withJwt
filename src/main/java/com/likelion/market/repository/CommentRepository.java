d6c1b8746ffd8ebf3f9441616f9fde7c221ea1a1
이희준 <syber911911@naver.com>
2023-07-05 19:25:34 +0900

    fix: CommentRepository  - 불필요한 Import 제거

--- src/main/java/com/likelion/market/repository/CommentRepository.java
+++ src/main/java/com/likelion/market/repository/CommentRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-    Page<CommentEntity> findAllByItemId(Long id, Pageable pageable);
+    Page<CommentEntity> findAllByItemId(Long ItemId, Pageable pageable);
 }
