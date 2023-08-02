580e7765b2c925fac81104c8b29b3137f53c0a4d
이희준 <syber911911@naver.com>
2023-08-02 22:47:57 +0900

    sql 문 삭제

--- src/main/resources/comment.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE comments;
-CREATE TABLE comments (
-    id       integer primary key autoincrement,
-    item_id  integer,
-    writer   text,
-    password text,
-    content  text,
-    reply    text
-);
